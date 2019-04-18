159dcf0d2c262cc6361a945cea47909fad7794cb
BbqCoder <korba.adam@gmail.com>
2019-04-18 14:59:48 +0200

    Uploading file to S3

--- test_django/pseudo_test/views.py
+++ test_django/pseudo_test/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-import boto3
+import boto3    
 
 def send_answer(request):
     if request.method == "POST":
@@ -8,10 +8,6 @@ def send_answer(request):
         if (uploaded_file.name[-4:] == ".txt" or uploaded_file.name[-4:] == ".pdc"
             ) and uploaded_file.size < 1000000:
             s3 = boto3.resource('s3')
-            for bucket in s3.buckets.all():
-                print(bucket.name)
-            print(uploaded_file.name)
-            print(uploaded_file.size)
-
+            s3.Bucket('pseudocodetester').put_object(Key='test.jpg', Body=uploaded_file)
 
     return render(request, 'pseudo_test/send_answer.html')
