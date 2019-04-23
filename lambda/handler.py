3c94bd51027f4882c0cae226be3fae65fdaa6988
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2019-04-23 18:58:03 +0200

    Add bucket env

--- lambda/handler.py
+++ lambda/handler.py
@@ -11,24 +11,23 @@ from pseudotest import test
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-s3 = boto3.client('s3')
+s3 = boto3.client("s3")
 
-BUCKET = "pseudotester"
+BUCKET = os.environ["S3_BUCKET"]
 
 
 def lambda_handler(event, context):
 
     logger.info(f"Using pseudo@{pseudo.__version__}")
-    logger.info('got event {}'.format(event))
+    logger.info("got event {}".format(event))
 
     KEY = event["Records"][0]["s3"]["object"]["key"]
-
 
     try:
         exercise, task_id = KEY.split("%40")
         logger.info(f"Exercise: {exercise}, Task_id: {task_id}")
     except ValueError:
-        logger.error('Invalid name: {}'.format(KEY))
+        logger.error("Invalid name: {}".format(KEY))
         raise
 
     KEY = f"{exercise}@{task_id}"
@@ -36,24 +35,22 @@ def lambda_handler(event, context):
     try:
         logger.info("Starting file download")
         response = s3.get_object(Bucket=BUCKET, Key=KEY)
-        pseudocode = response['Body'].read().decode('utf-8')
+        pseudocode = response["Body"].read().decode("utf-8")
         logger.info("File dowload finished")
         logger.info("Removing object frow queue")
         s3.delete_object(Bucket=BUCKET, Key=KEY)
         logger.info(f"Object {KEY} removed")
     except ClientError as e:
-        if e.response['Error']['Code'] == "404":
+        if e.response["Error"]["Code"] == "404":
             logger.error(f"The object {KEY} does not exist.")
         else:
             logger.error(str(e))
         raise
 
+    logger.info("Got pseudocode:\n" + pseudocode)
     logger.info("Running test...")
     results = test(exercise, pseudocode, logger)
 
     logger.info(results)
 
-    return {
-        'statusCode': 200,
-        'body': json.dumps(results)
-    }
+    return {"statusCode": 200, "body": json.dumps(results)}
