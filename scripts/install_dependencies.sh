99ae584ee0ab510fd28ba5526ddb165dc1e4d3f0
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2019-04-24 11:39:30 +0200

    Fix mysqlclient dependency

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+set -x
+set -ueo pipefail
+
 cd /home/ubuntu/pseudotest/test_django
 
 sudo apt-get update
-sudo apt-get install -y nginx
+sudo apt-get install -y default-libmysqlclient-dev
 pip3 install -r requirements.txt
--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
+set -x
+set -ueo pipefail
+
 cd /home/ubuntu/pseudotest/test_django
 
 DATE=$(date +%H-%M-%S-%d-%m-%Y)
 
-mkdir /var/log/pseudotest
+if ![[ -d /var/log/pseudotest ]]; then
+    mkdir /var/log/pseudotest
+fi
 touch /var/log/pseudotest/${DATE}.log
 
 gunicorn --pid gunicorn.pid --workers=2 --access-logfile /var/log/pseudotest/${DATE}.log -D test_django.wsgi -b 0.0.0.0:80
