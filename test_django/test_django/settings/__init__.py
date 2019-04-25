bec9311d3ebd856c4ec1fc24cf2b50c70ff750f7
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2019-04-25 09:06:15 +0200

    Add cors

--- test_django/test_django/settings/__init__.py
+++ test_django/test_django/settings/__init__.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "pseudo_test",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -40,6 +41,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "test_django.urls"
@@ -109,5 +111,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 S3_NAME = os.environ["S3_BUCKET"]
