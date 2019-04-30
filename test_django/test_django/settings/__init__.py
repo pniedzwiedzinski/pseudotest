34cb046708c13cec789313a6f70ed00c7bfc554b
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2019-04-30 18:38:50 +0200

    Fix static path

--- test_django/test_django/settings/__init__.py
+++ test_django/test_django/settings/__init__.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_DIR = os.path.normpath(os.path.join(BASE_DIR, "../"))
 
 if "STAGE" in os.environ and os.environ["STAGE"] == "prod":
     from .production import *
@@ -110,10 +111,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(PROJECT_DIR, "staticfiles")
 STATIC_URL = "/static/"
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATICFILES_DIRS = (os.path.join(PROJECT_DIR, "static"),)
 
 CORS_ORIGIN_ALLOW_ALL = True
 
