8ce4b849d9dbeba805b507677648e189c6239fcf
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2019-04-30 18:28:38 +0200

    Update paths

--- test_django/test_django/settings/__init__.py
+++ test_django/test_django/settings/__init__.py
@@ -110,10 +110,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(os.path.dirname(os.path.abspath(__file__)), "staticfiles")
 
-STATICFILES_DIRS = (os.path.join(os.path.dirname(os.path.abspath(__file__)), "static"),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 CORS_ORIGIN_ALLOW_ALL = True
 
