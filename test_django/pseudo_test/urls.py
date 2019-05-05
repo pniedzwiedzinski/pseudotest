9275204793eec5338585ee4935e094f3161e1db9
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2019-05-05 10:57:45 +0200

    Add static path

--- test_django/pseudo_test/urls.py
+++ test_django/pseudo_test/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 from rest_framework import routers
 
 router = routers.DefaultRouter()
-router.register('tasks', views.TaskView, base_name='TaskView')
+router.register("tasks", views.TaskView, base_name="TaskView")
 
-urlpatterns = [path("submit/", views.send_answer, name="submit"),
-               path("get/<file_id>", views.get_answer, name="get"),
-               path("", include(router.urls))]
+urlpatterns = [
+    path("submit/", views.send_answer, name="submit"),
+    path("get/<file_id>", views.get_answer, name="get"),
+    path("", include(router.urls)),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
