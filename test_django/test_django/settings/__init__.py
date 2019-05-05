5baf607f4e6ac0779dfefdab7ad3a1cad1bf3b86
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2019-05-05 10:42:52 +0200

    Rename static folder

--- test_django/test_django/settings/__init__.py
+++ test_django/test_django/settings/__init__.py
@@ -111,7 +111,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(PROJECT_DIR, "static")
 STATIC_URL = "/static/"
 
 STATICFILES_DIRS = (os.path.join(PROJECT_DIR, "static"),)
