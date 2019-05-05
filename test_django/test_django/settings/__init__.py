ee7dca12a4b12e748c0b19cfafe64b5e573dbc5e
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2019-05-05 18:09:55 +0200

    Add CORS

--- test_django/test_django/settings/__init__.py
+++ test_django/test_django/settings/__init__.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "pseudo_test",
+    "corsheaders",
     "rest_framework",
     "whitenoise",
 ]
@@ -44,6 +45,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "test_django.urls"
